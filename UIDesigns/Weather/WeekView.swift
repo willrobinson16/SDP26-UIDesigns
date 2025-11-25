555e20c163541ec0615905a44f700300844d8b0a
Guillermo Robinson <grobinson93@gmail.com>
2025-11-25 10:34:12 +0100

    Actualización Weather

---
+++ UIDesigns/Weather/WeekView.swift
@@ -0,0 +1,18 @@
+//
+//  WeekView.swift
+//  UIDesigns
+//
+//  Created by Guillermo Robinson on 25/11/25.
+//
+
+import SwiftUI
+
+struct WeekView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekView()
+}
